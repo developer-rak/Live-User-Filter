934208b9815b1a84b5e57b55da342590c9bbab93
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-08-14 18:13:44 +0500

    finilize

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const listItem = []
 
 getData()
 
+filter.addEventListener('input', (e) => filterData(e.target.value))
+
 async function getData() {
     const res = await fetch('https://randomuser.me/api?results=50')
 
@@ -12,9 +14,30 @@ async function getData() {
     } = await res.json()
 
     // clear results
-    results.innrerHTML = ''
+    result.innerHTML = ''
 
     results.forEach(user => {
+        const li = document.createElement('li')
 
+        listItem.push(li)
+
+        li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class="user-info">
+                <h4>${user.name.first}, ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}
+            </div>
+        `
+        result.appendChild(li)
+    })
+}
+
+function filterData(searchTerm) {
+    listItem.forEach(item => {
+        if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.remove('hide')
+        } else {
+            item.classList.add('hide')
+        }
     })
 }
